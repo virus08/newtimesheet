5b409797171d3e434a53c288f893247c0b391e93
Wanchai Fuangmali <Wanchai@vstecs.co.th>
2019-06-15 17:38:43 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="isAuthenticated" id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <h2><a href="#" v-on:click.stop="logOut()">Log out</a></h2>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+import authentication from './authentication'
+export default {
+  name: 'app',
+  computed: {
+    isAuthenticated() {
+      return authentication.isAuthenticated();
+    }
+  },
+  methods: {
+    logOut() {
+      authentication.signOut();
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
